904410d7f160c8779cc01e5a92de65cead13a388
Stephanie Villalobos <svillalobos2027@gmail.com>
2026-02-06 11:49:20 -0800

    Fe.6th video

--- LazyDemos/ContentView.swift
+++ LazyDemos/ContentView.swift
@@ -10,15 +10,24 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            
+            RedAndCyanView()
         }
         .padding()
+    }
+}
+struct RedAndCyanView: View {
+    var body: some View {
+        ZStack{
+            Color(.red)
+            Rectangle()
+                .fill(.cyan)
+                .frame(width: 100, height: 100)
+        }
     }
 }
 
 #Preview {
     ContentView()
 }
+
