a1eea7967339f04966aeb42821966f6168e32c45
Stephanie Villalobos <svillalobos2027@gmail.com>
2026-02-06 11:20:24 -0800

    Initial Commit

---
+++ LazyDemos/LazyDemosApp.swift
@@ -0,0 +1,17 @@
+//
+//  LazyDemosApp.swift
+//  LazyDemos
+//
+//  Created by VILLALOBOS, STEPHANIE on 2/6/26.
+//
+
+import SwiftUI
+
+@main
+struct LazyDemosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
